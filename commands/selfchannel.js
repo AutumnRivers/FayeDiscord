ea4a605e3f494fbf4414c163e48acdeff1f8fcf9
Autumn Rivers <natirivers@protonmail.ch>
2022-03-21 22:56:42 -0400

    Add new roles and selfchannels

--- commands/selfchannel.js
+++ commands/selfchannel.js
@@ -3,13 +3,11 @@ const fs = require('fs');
 async function giveAccess(message, channel) {
     const config = require(`../config/main.json`);
     const guildObject = global.fayeGuilds.cache.find(guild => guild.id == config.mainGuildID);
-    const channelObject = guildObject.channels.cache.find(ch => ch.id == channel.channelID);
+    const channelObject = guildObject.channels.cache.find(ch => ch.id == channel);
     const guildMembers = await guildObject.members.fetch();
     const memberObject = guildMembers.find(member => member.id == message.author.id);
 
     channelObject.permissionOverwrites.create(memberObject.id, { VIEW_CHANNEL: true }, { reason: 'Requested via Faye from selfchannel command', type: 1 });
-
-    message.author.send('Gave you access to the channel! Try it out: <#' + channel.channelID + '>\n\nIf you would like to remove access, please contact a moderator!');
 }
 
 module.exports = {
@@ -19,7 +17,7 @@ module.exports = {
     disableSlashCommand: true,
     dmOnly: true,
     example: "faye!selfchannel lgbtqia",
-    execute(message, args) {
+    async execute(message, args) {
         if(message.channel.type !== 'DM') return message.delete();
         if(!args[0]) return message.reply('Woah, hold on there! You do not have any arguments! Change that, and I will get back to you, yeah?');
 
@@ -29,6 +27,15 @@ module.exports = {
 
         if(!channel[0]) return message.reply('That self-channel does not exist!');
 
-        giveAccess(message, channel[0]);
+        if(channel[0].channelID.constructor.name == 'Array') {
+            for(const channelID of channel[0].channelID) {
+                giveAccess(message, channelID)
+            }
+
+            message.author.send('Gave you access to the channels! They should now be available to you under the "Special Channels" category! :)\n\nIf you would like to remove access, please contact a moderator!');
+        } else {
+            giveAccess(message, channel[0].channelID);
+            message.author.send('Gave you access to the channel! Try it out: <#' + channel[0].channelID + '>\n\nIf you would like to remove access, please contact a moderator!');
+        }
     }
 }
